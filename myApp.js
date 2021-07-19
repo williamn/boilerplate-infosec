4433b74bffe7cda9cfa50c30a7c1d8572fbeac80
williamn <27722+williamn@users.noreply.github.com>
2021-07-19 10:14:48 +0000

    feat: deny X-Frame-Options header

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ const express = require('express');
 const helmet = require("helmet");
 const app = express();
 app.use(helmet.hidePoweredBy());
+app.use(
+  helmet.frameguard({
+    action: "deny",
+  })
+);
 
 
 
